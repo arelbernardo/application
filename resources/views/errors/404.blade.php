b8f3b336acad3bc318e4b94cba4f164a347ebfd3
daryl bernardo <arelbernardo@gmail.com>
2017-08-25 16:27:50 +0800

    Remove Finance widgets as it will put into separate module

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+    <div class="err_exceptions">asdasd</div>
+@endsection
+
--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -4,8 +4,42 @@
 <div class="custom_fluid-container container-fluid">
     <div class="custom_home-head-banner">
     </div>
-    <div class="app_container col-xs-12 col-sm-12 col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2">
-        <div class="expenses_add-expense row">
+    <div class="app_container container">
+        <div id="organizer_container">
+            <div class="organizer_item_container col-sm-3">
+                <div class="organizer_item col-sm-12">
+                    <div class="organizer_banner">
+
+                    </div>
+                    <div class="organizer_title">Finance app</div>
+                </div>
+            </div>
+            <div class="organizer_item_container col-sm-3">
+                <div class="organizer_item col-sm-12">
+                    <div class="organizer_banner">
+
+                    </div>
+                    <div class="organizer_title">Note taking app</div>
+                </div>
+            </div>
+            <div class="organizer_item_container col-sm-3">
+                <div class="organizer_item col-sm-12">
+                    <div class="organizer_banner">
+
+                    </div>
+                    <div class="organizer_title">Scheduler app</div>
+                </div>
+            </div>
+            <div class="organizer_item_container col-sm-3">
+                <div class="organizer_item col-sm-12">
+                    <div class="organizer_banner">
+
+                    </div>
+                    <div class="organizer_title">Etc Etc</div>
+                </div>
+            </div>
+        </div>
+        {{--<div class="expenses_add-expense row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <div id="modal_add-expense" class="modal fade modal-xs" role="dialog" data-keyboard="false" data-focus="true"></div>
                 <div class="form-group pull-right">
@@ -17,12 +51,12 @@
                 </div>
             </div>
         </div>
-        @include('profile.partials.widgets')
+        @include('profile.partials.widgets')--}}
     </div>
 </div>
 @endsection
 @section('scripts')
 <script>
-    new ProfileManager('{{Auth::user()->username}}');
+    {{--new ProfileManager('{{Auth::user()->username}}');--}}
 </script>
 @endsection
