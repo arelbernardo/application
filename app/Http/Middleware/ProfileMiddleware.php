f727393c931e38daf0d439341b76c1e16766eb49
daryl bernardo <arelbernardo@gmail.com>
2017-08-25 16:24:29 +0800

    Add ProfileController for authenticating registered users.

---
+++ app/Http/Middleware/ProfileMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\User;
+use Closure;
+
+class ProfileMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $url = $request->url();
+        $user = explode('/',$url)[3];
+        if(User::where('username', $user)->count() === 0) {
+            return redirect('/');
+        }
+        return $next($request);
+    }
+}
